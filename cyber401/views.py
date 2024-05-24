a4ffc03a578fb8741ca6dd8b1ae82ab0523d2008
Allan Orina <orinaallan38@gmail.com>
2024-05-24 18:53:16 +0300

    Added Services Page

--- cyber401/views.py
+++ cyber401/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from datetime import datetime
 
 # Create your views here.
 def index(request):
-    template = loader.get_template('index.html')
+    current_year = datetime.now().year
+    context = {
+        'year': current_year,
+    }
+
+    return render(request, 'index.html', context)
+
+def services(request):
+    template = loader.get_template('services.html')
     return HttpResponse(template.render())
     
